b7879fbdb9ddf4b5219dac6c5ea225cbbb1ba085
shukhratjumayev <jumayevshuxratuzbek@gmail.com>
2025-07-21 13:15:07 +0500

    added lesson

---
+++ app.js
@@ -0,0 +1,8 @@
+function goToRegistration() {
+  window.location.href = 'registration.html';
+}
+
+  document.querySelector('.reg-btn').addEventListener('click', function () {
+    window.location.href = '.index.html';
+  });
+
